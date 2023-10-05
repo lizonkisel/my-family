a4e1988722d46bfdd930acf2995dbdfab292cfc4
lizonkisel <ees.hwork@gmail.com>
2023-10-05 18:59:38 +0300

    feat: add mask and first Person card version

--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./Main.css";
+import Person from "../Person/Person";
 
 function Main() {
-  return <main className="main" />;
+  return (
+    <main className="main">
+      <Person />
+    </main>
+  );
 }
 
 export default Main;
