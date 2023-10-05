927b576303e826eb2432b5aa947f07407c089557
lizonkisel <ees.hwork@gmail.com>
2023-10-05 21:46:14 +0300

    feat: add props to Person component

--- src/Person/Person.tsx
+++ src/Person/Person.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 import "./Person.scss";
 
-function Person() {
+interface IPerson {
+  imageLink?: string;
+  name?: string;
+  date?: string;
+}
+
+function Person({
+  imageLink = "https://www.coloradospecialties.com/wp-content/uploads/2013/12/Person.Ashley.jpg",
+  name = "Иван Иванович Иванов",
+  date = "01.01.1900 - 12.12.1999"
+}: IPerson) {
   return (
     <section className="person">
-      <img
-        className="outer"
-        src="https://assets.aboutamazon.com/dims4/default/0865493/2147483647/strip/true/crop/1919x1080+1+0/resize/1320x743!/quality/90/?url=https%3A%2F%2Famazon-blogs-brightspot.s3.amazonaws.com%2F2e%2Fda%2F7d8a64fc43e8b08b5cc802659fc3%2Fcanyousharekindlebooks-hero-v1.jpeg"
-        alt=""
-      />
-      <span className="person__name">Иван Иванович Иванов</span>
-      <span className="person__date">01.01.1900 - 12.12.1999</span>
+      <img className="person__image" src={imageLink} alt="person" />
+      <span className="person__name">{name}</span>
+      <span className="person__date">{date}</span>
     </section>
   );
 }
