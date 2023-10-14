6c7077f72d6b29ec224ebe201030e95956862fb4
lizonkisel <ees.hwork@gmail.com>
2023-10-14 10:33:51 +0300

    feat: add react-d3-tree lib/ It's useless here

--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./Main.css";
 
 import { Component } from "../Canvas/Canvas";
+import OrgChartTree from "../reactd3";
 // import Board from "../Board/Board";
 
 function Main() {
   return (
     <main className="main">
       <Component />
+      <OrgChartTree />
     </main>
   );
 }
