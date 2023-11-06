eb49eea5b9b9a1e0731aa59bc1ac1973739c736f
lizonkisel <ees.hwork@gmail.com>
2023-11-06 17:12:55 +0300

    feat: add card generation from data-file

--- src/FlowBoard/FlowBoard.tsx
+++ src/FlowBoard/FlowBoard.tsx
@@ -10,20 +10,45 @@ import "../Person/Person.scss";
 
 import NodePerson from "../Person/NodePerson";
 
-const initialNodes = [
-  {
-    id: "node-1",
-    type: "textUpdater",
-    position: { x: 0, y: 0 },
-    data: { value: 123 }
-  },
-  {
-    id: "node-2",
-    type: "textUpdater",
-    position: { x: 200, y: 200 },
-    data: { value: 123 }
-  }
-];
+import data from "../data/data.json";
+
+// const initialNodes = [
+//   {
+//     id: "node-1",
+//     type: "textUpdater",
+//     position: { x: 0, y: 0 },
+//     data: { value: 123 }
+//   },
+//   {
+//     id: "node-2",
+//     type: "textUpdater",
+//     position: { x: 200, y: 200 },
+//     data: { value: 123 }
+//   }
+// ];
+
+const createNodes = (testData: any) => {
+  const nodes = testData.map((node: any) => {
+    let dateOfDeath;
+    if (node.date_of_death === null) {
+      dateOfDeath = "н.в.";
+    } else {
+      dateOfDeath = node.date_of_death;
+    }
+    return {
+      id: `node-${node.id}`,
+      type: "textUpdater",
+      position: { x: 0, y: 0 },
+      data: {
+        personName: `${node.name} ${node.patronymic} ${node.surname}`,
+        date: `${node.date_of_birth} - ${dateOfDeath}`
+      }
+    };
+  });
+  return nodes;
+};
+
+const initialNodes = createNodes(data);
 
 const initialEdges = [
   {
--- src/Person/NodePerson.tsx
+++ src/Person/NodePerson.tsx
@@ -2,19 +2,27 @@ import React from "react";
 import { Handle, Position } from "reactflow";
 import "./Person.scss";
 
+// interface IPerson {
+//   imageLink?: string;
+//   personName?: string;
+//   date?: string;
+// }
+
 interface IPerson {
-  imageLink?: string;
-  name?: string;
-  date?: string;
-  // data?: any;
-  isConnectable?: any;
+  data: {
+    imageLink?: string;
+    personName?: string;
+    date?: string;
+  };
+  isConnectable: any;
 }
 
 function NodePerson({
-  imageLink = "https://www.coloradospecialties.com/wp-content/uploads/2013/12/Person.Ashley.jpg",
-  name = "Иван Иванович Иванов",
-  date = "01.01.1900 - 12.12.1999",
-  // data,
+  data: {
+    imageLink = "https://www.coloradospecialties.com/wp-content/uploads/2013/12/Person.Ashley.jpg",
+    personName = "Иван Иванович Иванов",
+    date = "01.01.1900 - 12.12.1999"
+  },
   isConnectable
 }: IPerson) {
   return (
@@ -27,7 +35,7 @@ function NodePerson({
       />
       <section className="person">
         <img className="person__image" src={imageLink} alt="person" />
-        <span className="person__name">{name}</span>
+        <span className="person__name">{personName}</span>
         <span className="person__date">{date}</span>
       </section>
       <Handle
