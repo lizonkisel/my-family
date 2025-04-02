7d82f387d54261dbf2811d5e777366efa921fc87
lizonkisel <ees.hwork@gmail.com>
2025-04-02 14:32:24 +0300

    feat: add new args to countX function

--- src/utils/count-position.tsx
+++ src/utils/count-position.tsx
@@ -1,41 +1,4 @@
-const countX = (person: any) => {
-  // console.log(`Person.id: ${person.id}`);
-  let xCoor = 0;
-
-  const generation = person.generation;
-  console.log(`${generation}: generation`);
-
-  const gender = person.gender === "female" ? -0.5 : 1;
-  // console.log(`${gender}: gender`);
-
-  let childrenCoef = 0;
-  let partnerCoef = 0;
-
-  if (person.children.length !== 0) {
-    if (gender === -0.5) {
-      childrenCoef = person.id - person.children[0];
-    } else {
-      childrenCoef = person.id - person.children[0];
-    }
-    // childrenCoef = person.id - person.children[0];
-  }
-  // console.log(`${childrenCoef}: childrenCoef`);
-
-  if (person.partner.length !== 0) {
-    partnerCoef = person.id - person.partner[0];
-  }
-  // console.log(`${partnerCoef}: partnerCoef`);
-
-  xCoor = -1 * childrenCoef * 300 - gender * 300 - partnerCoef * -1 * 300;
-  // console.log(`${xCoor}: xCoor`);
-  return xCoor;
-};
-
-// const countY = (person: any) => {
-//   const yCoor = 0;
-//   return yCoor;
-// };
-
+let dataForNodes;
 interface IMainPersonData {
   id: number;
   name: string;
@@ -55,6 +18,68 @@ interface IGenerationsObj {
   [key: `gen-${number}`]: IMainPersonData[];
 }
 
+const countX = (
+  person: IMainPersonData,
+  j: number,
+  prevNodePosition: { x: number; y: number } | 0
+) => {
+  // console.log(`Person.id: ${person.id}`);
+  console.log(`id: ${person.id}`);
+  // j - поколение
+  console.log(`j: ${j}`);
+  console.log(`prevNodePosition: ${prevNodePosition}`);
+  let xCoor = 0;
+
+  if (j === 0) {
+    xCoor = 0;
+    return xCoor;
+  }
+
+  // Если это не первый айди. по идее должна срабатывать только для id = 1 (Валера). Но пока не так
+  if (prevNodePosition !== 0 && prevNodePosition.x === 0) {
+    console.log(`Ловушка для ${person.id}`);
+    xCoor = 200;
+    return xCoor;
+  }
+
+  // Если есть дети
+  if (prevNodePosition !== 0 && person.children.length !== 0) {
+    // Если ребёнок - это предыдущий айдишник
+    if (person.children[person.children.length - 1] === j - 1) {
+      xCoor = prevNodePosition.x - 300;
+      console.log(`id: ${person.id}, xCoor: ${xCoor}`);
+      // Если партнёр - предыдущий айдишник
+    } else if (person.partner.length !== 0 && person.partner[0] === j - 1) {
+      xCoor = prevNodePosition.x + 400;
+    } else {
+      xCoor = prevNodePosition.x - 800;
+    }
+  }
+
+  // Если нет детей и нет партнёра
+  if (
+    prevNodePosition !== 0 &&
+    person.children.length === 0 &&
+    person.partner.length === 0
+  ) {
+    xCoor = prevNodePosition.x - 600;
+  }
+
+  console.log(`id: ${person.id}, xCoor: ${xCoor}`);
+  return xCoor;
+};
+
+// const countY = (person: IMainPersonData, j: number) => {
+//   let yCoor = 0;
+
+//   if (j === 0) {
+//     yCoor = 0;
+//     return yCoor;
+//   }
+//   yCoor = 400 * person.generation;
+//   return yCoor;
+// };
+
 const calculateCoordinates = (data: IGenerationsObj) => {
   console.log(data);
   // Создам глубокую копию исходных данных
@@ -64,8 +89,15 @@ const calculateCoordinates = (data: IGenerationsObj) => {
   keys.forEach((key: any) => {
     const currGen = copyData[key];
     for (let j = 0; j < currGen.length; j++) {
-      const xCoor = countX(currGen[j]);
+      let prevPosition;
+      if (j === 0) {
+        prevPosition = 0;
+      } else {
+        prevPosition = currGen[j - 1].nodeData.position;
+      }
+      const xCoor = countX(currGen[j], j, prevPosition);
       const yCoor = 400 * currGen[j].generation;
+      // const yCoor = countY(currGen[j], j);
 
       const nodeData = {
         id: `node-${currGen[j].id}`,
@@ -80,8 +112,11 @@ const calculateCoordinates = (data: IGenerationsObj) => {
     }
   });
   console.log(copyData);
+  dataForNodes = copyData;
   return copyData;
 };
+
+console.log(dataForNodes);
 
 /* eslint-disable-next-line */
 export { calculateCoordinates };
