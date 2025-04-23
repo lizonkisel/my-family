4e57bdbf46d2f6b3c4b005f59de029718d0937c3
lizonkisel <ees.hwork@gmail.com>
2025-04-23 14:40:50 +0300

    feat: add right props processing for CardPerson. Add popup

--- src/components/CardWrapper/CardWrapper.tsx
+++ src/components/CardWrapper/CardWrapper.tsx
@@ -1,14 +1,46 @@
 import React from "react";
-// import CardPerson from "../CardPerson/CardPerson";
-import { getActiveCard } from "../../services/slices/activeCardSlice";
-import { useAppSelector } from "../../services/app/hooks";
+import "./CardWrapper.scss";
+import closeIcon from "../../img/close.svg";
+import CardPerson from "../CardPerson/CardPerson";
+import {
+  getActiveCard,
+  setActiveCard
+} from "../../services/slices/activeCardSlice";
+import { useAppSelector, useAppDispatch } from "../../services/app/hooks";
 
 export default function CardWrapper() {
   const activeCard = useAppSelector(getActiveCard);
+  const dispatch = useAppDispatch();
   console.log(activeCard);
+
+  const closePopup = () => {
+    dispatch(setActiveCard(undefined));
+  };
+
+  const closePopupByOuterClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (e.target === e.currentTarget) {
+      closePopup();
+    }
+  };
+
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+      closePopup();
+    }
+  });
+
   if (activeCard) {
-    // return <CardPerson personId={activeCard} />;
-    <div> </div>;
+    return (
+      /* eslint-disable-next-line */
+      <section className="popup" onClick={closePopupByOuterClick}>
+        <div className="popup__content">
+          <CardPerson personId={activeCard} />;
+          <button className="popup__close" type="button" onClick={closePopup}>
+            <img src={closeIcon} alt="cross" className="close__img" />
+          </button>
+        </div>
+      </section>
+    );
   }
   return <div> </div>;
 }
