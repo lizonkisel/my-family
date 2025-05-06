70ab9ed3843a81cd255c8dbcb10fc721dc6582e7
lizonkisel <ees.hwork@gmail.com>
2025-05-06 14:56:56 +0300

    fix: add Swiper to all Cards

--- src/components/CardGallery/CardGallery.tsx
+++ src/components/CardGallery/CardGallery.tsx
@@ -20,28 +20,30 @@ export default function CardGallery({ person }: { person: IPersonAllData }) {
     person.main_image.indexOf("/")
   );
 
-  // const list = require.context(`../../img/${folderRoute}`, false, /\.(png|jpg)$/).keys();
   const list = require.context(`../../img`, true, /\.(png|jpg)$/).keys();
 
-  const imagesArr = list.filter((img) => {
+  let imagesArr = list.filter((img) => {
     return img.indexOf(folderRoute) !== -1;
   });
 
-  console.log(imagesArr);
+  // Вот эта штука нужна, чтобы не брать все подряд картинки для пользователя без картинок.
+  // Благодаря массиву  imagesArr = ["./"], в качестве картинки карточки будет отображаться заглушка
+  if (folderRoute === "") {
+    imagesArr = ["./"];
+  }
 
   const listItems = imagesArr.map((img) => {
     const slicedRoute = img.slice(2);
+    console.log(slicedRoute);
 
     const { image } = useImage(`${slicedRoute}`);
-    // return <img className="gallery__item" src={image} alt="" key={v4()} />;
     return (
-      <SwiperSlide>
-        <img className="gallery__item" src={image} alt="" key={v4()} />
+      <SwiperSlide key={v4()}>
+        <img className="gallery__item" src={image} alt="" />
       </SwiperSlide>
     );
   });
 
-  // return <div className="keen-slider"> {listItems} </div>;
   return (
     <Swiper
       slidesPerView={1}
@@ -53,15 +55,6 @@ export default function CardGallery({ person }: { person: IPersonAllData }) {
       modules={[Pagination]}
       className="mySwiper"
     >
-      {/* <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-      <SwiperSlide>Slide 5</SwiperSlide>
-      <SwiperSlide>Slide 6</SwiperSlide>
-      <SwiperSlide>Slide 7</SwiperSlide>
-      <SwiperSlide>Slide 8</SwiperSlide>
-      <SwiperSlide>Slide 9</SwiperSlide> */}
       {listItems}
     </Swiper>
   );
